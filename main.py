6ac2b3be209827cc5617a49962749532199b30cb
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2020-07-05 14:52:23 -0500

    Fixed XPATH selector for publish button again

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ class Org:
 
         # press the publish button
         try:
-            y = "//div[(contains(text(), 'Publish') and not(contains(text(), 'after')))]"
+            y = "//div[(contains(string(), 'Publish') and not(contains(string(), 'after')))]"
             #browser.find_element_by_xpath("//div/span/button").click()
             browser.find_element_by_xpath(y).click()
         except Exception as e:
